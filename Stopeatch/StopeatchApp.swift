c41113953597b07e6e7d0f953e88b01a90b6e8fc
Kawarai-S <kayawakakururu@icloud.com>
2023-08-30 07:26:24 +0900

    Initial Commit

---
+++ Stopeatch/StopeatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  StopeatchApp.swift
+//  Stopeatch
+//
+//  Created by 瓦井つばさ on 2023/08/30.
+//
+
+import SwiftUI
+
+@main
+struct StopeatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
